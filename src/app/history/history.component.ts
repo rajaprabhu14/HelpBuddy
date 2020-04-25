37c170aab7514c84023129531bf7a8949d58ad95
prabhurajaprabhu <40233019+prabhurajaprabhu@users.noreply.github.com>
2020-04-25 01:30:12 +0100

    Commit changes
    commit changes

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  templateUrl: './history.component.html'
+  templateUrl: "./history.component.html"
 })
 
 export class HistoryComponent implements OnInit {
